22b9f223313a66443e770d309335c1920cf3ae92
角田和美 <tuna.oisi4@gmail.com>
2016-10-12 23:55:40 +0900

    loading画面作成工程1

--- src/load.js
+++ src/load.js
@@ -1,6 +1,4 @@
-require("./style.scss");
-
-$(function() {
+/*$(function() {
   var h = $(window).height();
 
   $('#wrap').css('display','none');
@@ -8,16 +6,13 @@ $(function() {
 });
 
 $(window).load(function () { //全ての読み込みが完了したら実行
-
   $('#loader-bg').delay(900).fadeOut(800);
   $('#loader').delay(600).fadeOut(300);
   $('#wrap').css('display', 'block');
-  //debugger
-  //$('#loader-bg').removeAttr('style');
-});
+});*/
 
 //10秒たったら強制的にロード画面を非表示
-$(function(){
+/*$(function(){
   setTimeout('stopload()',10000);
 });
 
@@ -25,4 +20,4 @@ function stopload(){
   $('#wrap').css('display','block');
   $('#loader-bg').delay(900).fadeOut(800);
   $('#loader').delay(600).fadeOut(300);
-}
+}*/
